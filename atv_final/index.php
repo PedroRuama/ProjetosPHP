9f0d757b1b7109fd5ab332cc1c62e233f2174ccf
PedroRuama <ruama246@gmail.com>
2023-06-20 22:33:40 -0300

    css

--- atv_final/index.php
+++ atv_final/index.php
@@ -5,28 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atv-Final</title>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-    <div>
-        <form name='login' method="post" >
-            login:
-            <input type="text" name="login"> <br><br>
-            Senha:
-            <input type="password" name="senha"> <br><br>
-            <input type="submit" value="Enviar" >
-        </form>
-        <?php
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                if($_POST['login'] == 'phpds' &  $_POST['senha'] == '123456'){
-                    header("Location: adm.php");
-                    die();
+    <div id="align">
+        <div id="div_form">
+            <form name='login' method="post" >
+                Login: <br>
+                <input type="text" name="login"> <br><br>
+                Senha: <br>
+                <input type="password" name="senha"> <br><br>
+                <input type="submit" value="Enviar" >
+            </form>
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    if($_POST['login'] == 'phpds' &  $_POST['senha'] == '123456'){
+                        header("Location: adm.php");
+                        die();
+                    }
+                    else{
+                        echo 'login incorreto';   
+                    }
                 }
-                else{
-                    echo 'login incorreto';   
-                }
-            }
-            ?>
-       
+                ?>
+           
+        </div>
     </div>
 </body>
 </html>
