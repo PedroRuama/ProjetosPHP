5c58cccb5f025279f9220983a85e834261d55d79
Edna Maria Bellix Castanho <ednacastanho@bentoquirino.edu>
2023-06-21 12:42:57 -0300

    css3

--- atv_final/index.php
+++ atv_final/index.php
@@ -21,23 +21,24 @@
                
                 <input type="text" name="login" placeholder="Login"> <br><br>
                 
-                <input type="password" name="senha" placeholder="Senha"> <br><br><br><br><br>
+                <input type="password" name="senha" placeholder="Senha"> <br><br>
+                <?php
+                    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                        if($_POST['login'] == 'phpds' &  $_POST['senha'] == '123456'){
+                            header("Location: adm.php");
+                            die();
+                        }
+                        else{
+                            echo '<div id="erro"> Login incorreto </div>';   
+                        }
+                    }
+                    ?>
+                <br><br><br><br><br>
                 <div id="div_enviar">
                     <input type="submit" value="Enviar" id="enviar">
     
                 </div>
             </form>
-            <?php
-                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                    if($_POST['login'] == 'phpds' &  $_POST['senha'] == '123456'){
-                        header("Location: adm.php");
-                        die();
-                    }
-                    else{
-                        echo 'login incorreto';   
-                    }
-                }
-                ?>
            
         </div>
     </div>
