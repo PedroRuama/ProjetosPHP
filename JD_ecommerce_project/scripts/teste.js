9c43c5d5ac47088c43b100cd0f27b230cf7535c1
PedroRuama <ruama246@gmail.com>
2024-03-01 17:35:45 -0300

    front cabeçalho/navbar lateral

---
+++ JD_ecommerce_project/scripts/teste.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var openBtn = document.querySelector('.open-btn');
+  var closeBtn = document.querySelector('.close-btn');
+  var sidenav = document.querySelector('.sidenav');
+
+  openBtn.addEventListener('click', function() {
+    sidenav.style.width = '250px';
+  });
+
+  closeBtn.addEventListener('click', function() {
+    sidenav.style.width = '0';
+  });
+});
